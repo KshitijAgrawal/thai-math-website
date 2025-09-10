ba6b5632f261122c54e4cdeb3ecdcf5ed3c600d6
KshitijAgrawal <kshtagrawal@gmail.com>
2025-09-09 22:43:20 -0700

    remove the disdir to fallback to .next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     trailingSlash: true,
     images: {
       unoptimized: true
-    },
-    distDir: 'build'
+    }
   }
   
   module.exports = nextConfig
